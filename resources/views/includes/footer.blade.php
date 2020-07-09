31d887e01cb5f64f4e8d78d180749ac8899d87a0
Alex <gageviolo@gmail.com>
2020-07-10 02:58:01 +0500

    Add copyright footer string

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,28 +1,44 @@
 @yield('footer')
 
 <div class="container">
-    <div>
-        <div><a href="{{ route('page.about') }}">О нас</a></div>
-        <div><a href="{{ route('page.contact') }}">Контакты</a></div>
+    <div class="widgets">
+        <div>
+            <div><a href="{{ route('page.about') }}">О нас</a></div>
+            <div><a href="{{ route('page.contact') }}">Контакты</a></div>
 
+        </div>
+
+
+        <div>
+            @guest
+                {{--<div><a href="{{ route('register.form') }}">Регистрация</a></div>--}}
+                {{--<div><a href="{{ route('login.form') }}">Вход</a></div>--}}
+            @endguest
+
+            @auth
+                <div><a href="{{ route('dashboard') }}">Аккаунт</a></div>
+                <div><a href="/logout">Выйти</a></div>
+            @endauth
+        </div>
+
+        <div>
+            <div><a href="/post">Все записи</a></div>
+            <div><a href="/category">Все категории</a></div>
+        </div>
     </div>
 
+    <div class="copyrights">
+        @php
+            $date = date('Y');
+        @endphp
 
-    <div>
-        @guest
-            {{--<div><a href="{{ route('register.form') }}">Регистрация</a></div>--}}
-            {{--<div><a href="{{ route('login.form') }}">Вход</a></div>--}}
-        @endguest
-
-        @auth
-            <div><a href="{{ route('dashboard') }}">Аккаунт</a></div>
-            <div><a href="/logout">Выйти</a></div>
-        @endauth
-    </div>
-
-    <div>
-        <div><a href="/post">Все записи</a></div>
-        <div><a href="/category">Все категории</a></div>
+        <div>
+            © Все права защищены 2014 - {{ $date }}. Qukenon.ru
+            <br>
+            Копировать материалы разрешается только с указанием открытой индексируемой ссылки на источник Qukenon.ru
+        </div>
     </div>
 
 </div>
+
+
