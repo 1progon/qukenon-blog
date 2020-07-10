a68b6e0b78855e3d349165c0c332261431135fe7
Alex <gageviolo@gmail.com>
2020-07-10 22:02:29 +0500

    Fix: о нас -> о сайте + Add: About, Contact pages контент

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -13,6 +13,36 @@
     <div class="container">
         <div class="single-page">
             <h1>Контакты</h1>
+
+            <div>
+                <p>Привет - это Qukenon, для связи со мной отправь сообщение в форме ниже</p>
+
+                <h2>Отправить сообщение</h2>
+                <form action="/contact" method="post">
+                    <div class="input-group">
+                        <label for="email">E-mail</label>
+                        <input type="text" name="email" id="email">
+                    </div>
+
+                    <div class="input-group">
+                        <label for="name">Имя</label>
+                        <input type="text" name="name" id="name">
+                    </div>
+
+                    <div class="input-group">
+                        <label for="message">Сообщение</label>
+                        <textarea name="message" id="message" rows="5"></textarea>
+                    </div>
+
+                    <div class="input-group">
+                        <input type="button" name="" id="" value="Отправить">
+                    </div>
+
+
+
+
+                </form>
+            </div>
         </div>
     </div>
 
