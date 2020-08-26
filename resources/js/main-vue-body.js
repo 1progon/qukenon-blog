0c158ac9f4247f42a45f93db429ea739470536bd
Alex <gageviolo@gmail.com>
2020-08-26 13:55:09 +0500

    Remove scroll to top script and move him to main vue js

--- resources/js/main-vue-body.js
+++ resources/js/main-vue-body.js
@@ -14,4 +14,24 @@ new Vue({
         'post-contents': PostContents
     },
 
+    created() {
+        window.addEventListener('load', this.showToTopButton);
+        window.addEventListener('scroll', this.showToTopButton);
+    },
+
+    methods: {
+        showToTopButton() {
+            let button = document.getElementsByClassName('to-top-button')[0];
+            removeEventListener('load', this)
+
+            let scrolledHeight = document.documentElement.scrollTop;
+
+            if (scrolledHeight > 200) {
+                button.classList.add('show');
+            } else {
+                button.classList.remove('show');
+            }
+        }
+    }
+
 });
